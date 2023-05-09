e77d6bc782c3ac9f3e9d9489305476a1a77c836c
sinha28shivam <sinha28.shivam@gmail.com>
2023-05-09 18:36:21 +0530

    Changes. in main.css and and apply on the dashboard home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ErrorPageComponent } from './MyComponents/error-page/error-page.component';
 import { WalletDetailsComponent } from './MyComponents/wallet-details/wallet-details.component';
 import { WalletManagementComponent } from './MyComponents/wallet-management/wallet-management.component';
+import { DashboardHomeComponent } from './MyComponents/dashboard-home/dashboard-home.component';
 
 // import { NavbarComponent } from './MyComponents/navbar/navbar.component';
 
 const routes: Routes = [
-  {path: 'error', component: ErrorPageComponent},
+  // {path: 'error', component: ErrorPageComponent},
+  {path:'',component:DashboardHomeComponent},
   {path: 'wallet-details', component: WalletDetailsComponent},
   {path: '**', component: ErrorPageComponent},
   {path: 'WalletManagement', component: WalletManagementComponent},
