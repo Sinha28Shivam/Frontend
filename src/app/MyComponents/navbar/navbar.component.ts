e8514f11ebfc94d31bbe241dcb09eb0171353a26
sinha28shivam <shivam.sinha@hcl.com>
2023-05-12 15:56:29 +0530

    commented empty constructor.

--- src/app/MyComponents/navbar/navbar.component.ts
+++ src/app/MyComponents/navbar/navbar.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
   constructor(){ }
-<!--   ngOnInit(): void {
-  } -->
+// <!--   ngOnInit(): void {
+//   } -->
 
 
 }
